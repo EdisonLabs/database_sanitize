4dbc823e92c06ce91616ed52ff0db9eb9c46409b
Malcolm Young <malcolm@rubydesign.co.uk>
2018-12-04 17:53:28 +0000

    fix coding standards issues mentioned by codesniffer

--- src/Commands/DatabaseSanitizeCommands.php
+++ src/Commands/DatabaseSanitizeCommands.php
@@ -20,7 +20,9 @@ class DatabaseSanitizeCommands extends DrushCommands {
   /**
    * Compares existing database.sanitize.yml files on the site installation against existing database tables.
    *
-    * @param array $options An associative array of options whose values come from cli, aliases, config, etc.
+   * @param array $options
+   *   An associative array of options whose values come from cli, aliases, config, etc.
+   *
    * @option file
    *   The full path to a sanitize YML file.
    * @option list
@@ -29,7 +31,7 @@ class DatabaseSanitizeCommands extends DrushCommands {
    * @command db:sanitize-analyze
    * @aliases dbsa,db-sanitize-analyze
    */
-  public function sanitizeAnalyze(array $options = ['file' => null, 'list' => null]) {
+  public function sanitizeAnalyze(array $options = ['file' => NULL, 'list' => NULL]) {
     // See bottom of https://weitzman.github.io/blog/port-to-drush9 for details on what to change when porting a
     // legacy command.
   }
@@ -37,7 +39,9 @@ class DatabaseSanitizeCommands extends DrushCommands {
   /**
    * Generates a database.sanitize.yml file for tables not specified on sanitize YML files.
    *
-    * @param array $options An associative array of options whose values come from cli, aliases, config, etc.
+   * @param array $options
+   *   An associative array of options whose values come from cli, aliases, config, etc.
+   *
    * @option file
    *   The full path to a sanitize YML file.
    * @option machine-name
@@ -46,7 +50,7 @@ class DatabaseSanitizeCommands extends DrushCommands {
    * @command db:sanitize-generate
    * @aliases dbsg,db-sanitize-generate
    */
-  public function sanitizeGenerate(array $options = ['file' => null, 'machine-name' => null]) {
+  public function sanitizeGenerate(array $options = ['file' => NULL, 'machine-name' => NULL]) {
     // See bottom of https://weitzman.github.io/blog/port-to-drush9 for details on what to change when porting a
     // legacy command.
   }
